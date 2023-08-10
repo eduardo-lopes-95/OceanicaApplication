9461f4d6d513a72ef4455dc7d084d417b32b61e4
Eduardo Sabino <eduardo.lopes-ext@telecall.com>
2023-08-09 23:28:27 -0300

    refator

--- Oceanica.API/Program.cs
+++ Oceanica.API/Program.cs
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
+app.UseCors("AllowOrigin");
 
 app.MapControllerRoute(
     name: "default",
